93cb1d723947e5fbdbc3e615d8caf82c6afa57fa
saifihaji3765-cmd <saifihaji3765@gmail.com>
2025-12-28 15:20:49 +0530

    Delete run_local.sh

--- run_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r backend/requirements.txt
-export FLASK_APP=backend/app.py
-export FLASK_ENV=development
-flask db upgrade || true
-python backend/app.py
